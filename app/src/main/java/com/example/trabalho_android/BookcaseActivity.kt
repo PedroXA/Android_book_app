0316adb2a485581d4500554a53823fd241fa1b75
Pedro <pedroxad@gmail.com>
2023-05-13 11:04:31 -0300

    Modificação 13/05/2023 -- Correção de bugs

--- app/src/main/java/com/example/trabalho_android/BookcaseActivity.kt
+++ app/src/main/java/com/example/trabalho_android/BookcaseActivity.kt
@@ -2,10 +2,13 @@ package com.example.trabalho_android
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class BookcaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_app)
+
+
     }
 }
